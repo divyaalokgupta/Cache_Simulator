8d5fd3c6dc7490ec851bcb1c11e2bf3fed4159d9
divyaalokgupta <taruntanuj007@gmail.com>
2015-09-26 20:31:22 -0400

    Code working wel for Validation Run 4. Showing matching simulation statistics as well. Coded the LRU sequenced output. Now need t start with L2.

--- cache_sim.c
+++ cache_sim.c
@@ -28,6 +28,8 @@ struct cache {
         unsigned int read_misses;
         unsigned int write_hits;
         unsigned int write_misses;
+        unsigned int writebacks;
+        char trace[100];
         struct cache_block *blocks;
         struct cache *next;
 };
@@ -60,15 +62,6 @@ int main(int argc, char *argv[])
                 return 0;
         }
 
-/*       printf("%s\n",argv[1]);
-        printf("%s\n",argv[2]);
-        printf("%s\n",argv[3]);
-        printf("%s\n",argv[4]);
-        printf("%s\n",argv[5]);
-        printf("%s\n",argv[6]);
-        printf("%s\n",argv[7]);
-*/
-
         unsigned int blocksize = atoi(argv[1]);
         unsigned int L1_size = atoi(argv[2]);
         unsigned int L1_assoc = atoi(argv[3]);
@@ -79,6 +72,7 @@ int main(int argc, char *argv[])
         struct cache *L1 = (struct cache*)malloc(sizeof(struct cache));
         L1 = create_cache(blocksize,L1_assoc,L1_size);
 
+        strcpy(L1->trace,argv[7]);
         FILE *trace = fopen(argv[7],"r");
         if(trace == NULL)
         {
@@ -97,7 +91,6 @@ int main(int argc, char *argv[])
 
                 for(i=2;i<=strlen(str);i++)
                 {
-//                      printf("Str: %s,Address: %s\n",str,address);
                         address[i-2] = str[i];
                 }
                 address[i-2] = '\0';
@@ -138,13 +131,14 @@ struct cache *create_cache(unsigned int blocksize, unsigned int assoc, unsigned 
         mem->read_misses = 0;
         mem->write_hits = 0;
         mem->write_misses = 0;
+        mem->writebacks = 0;
         mem->next = NULL;
-//        printf("%d rows, %d way associative, %d bytes in each block, Index %d bits, Tag %d bits, Block Offset %d bits\n",mem->rows,mem->assoc, mem->blocksize, mem->index_bits,mem->tag_bits,mem->block_offset_bits);
+/*      printf("%d rows, %d way associative, %d bytes in each block, Index %d bits, Tag %d bits, Block Offset %d bits\n",mem->rows,mem->assoc, mem->blocksize, mem->index_bits,mem->tag_bits,mem->block_offset_bits); */
         
         mem->blocks = (struct cache_block*)malloc(sizeof(struct cache_block) * mem->rows * mem->assoc);
-//        printf("Allocated %d Bytes\n",sizeof(struct cache_block)*mem->rows*mem->assoc);
+/*      printf("Allocated %d Bytes\n",sizeof(struct cache_block)*mem->rows*mem->assoc); */
         
-//Setting valid bits for all blocks to zero
+/* Setting valid bits for all blocks to zero */
         unsigned int i;
         for(i=0;i<mem->tot_numblocks;i++)
         {
@@ -164,30 +158,79 @@ void delete_cache(struct cache *mem)
 
 void print_cache(struct cache *mem)
 {
+        printf("===== Simulator Configuration =====\n");
         printf("  BLOCKSIZE:\t\t%d\n",mem->blocksize);
         printf("  L1_SIZE:\t\t%d\n",mem->tot_size);
         printf("  L1_ASSOC:\t\t%d\n",mem->assoc);
         printf("  VC_NUM_BLOCKS:\t%d\n",0);
         printf("  L2_SIZE:\t\t%d\n",0);
         printf("  L2_ASSOC:\t\t%d\n",0);
-        printf("  trace_file:\t\n");
+        printf("  trace_file:\t%s\n",mem->trace);
+        printf("\n");
 
+        printf("===== L1 contents =====\n");
         unsigned int i,j;
         for(i=0;i<mem->rows;i++)
         {
             printf("  set   %d:   ",i);
+
+            unsigned int validblocks = 0, min = 0;
             for (j=0;j<mem->assoc;j++)
-            {   //Need to write code for sorting based on LRU
+            {
                 if (mem->blocks[i*mem->assoc+j].valid == 1)
+                {
+                    validblocks++;
+                }
+            }
+
+            for (j=0;j<mem->assoc;j++)
+            {
+                if(mem->blocks[i*mem->assoc+j].valid == 1 && mem->blocks[i*mem->assoc+j].count == min)
                 {
                     if (mem->blocks[i*mem->assoc+j].dirty == 0)
                         printf("%lx    ",mem->blocks[i*mem->assoc+j].tag);
                     else
                         printf("%lx D  ",mem->blocks[i*mem->assoc+j].tag);
+                    min++;
+
+                    if(min < validblocks)
+                        j = 0;
+                    else
+                        break;
                 }
             }
+
+//            for (j=0;j<mem->assoc;j++)
+//            {   //Need to write code for sorting based on LRU
+//                
+//                if (mem->blocks[i*mem->assoc+j].valid == 1)
+//                {
+//                    if (mem->blocks[i*mem->assoc+j].dirty == 0)
+//                        printf("%lx    ",mem->blocks[i*mem->assoc+j].tag);
+//                    else
+//                        printf("%lx D  ",mem->blocks[i*mem->assoc+j].tag);
+//                }
+//            }
             printf("\n");
         }
+        
+        printf("\n ===== Simulation results =====\n");
+        printf(" a. number of L1 reads:                       %d\n",mem->read_hits+mem->read_misses);
+        printf(" b. number of L1 read misses:                 %d\n",mem->read_misses);
+        printf(" c. number of L1 writes:                      %d\n",mem->write_hits+mem->write_misses);
+        printf(" d. number of L1 write misses:                %d\n",mem->write_misses);
+        printf(" e. number of swap requests:                      0\n");
+        printf(" f. swap request rate:                       0.0000\n");
+        printf(" g. number of swaps:                              0\n");
+        printf(" h. combined L1+VC miss rate:                %0.4f\n",(float)(mem->read_misses+mem->write_misses)/(mem->read_hits+mem->read_misses+mem->write_hits+mem->write_misses));
+        printf(" i. number writebacks from L1/VC:                 %d\n",mem->writebacks);
+        printf(" j. number of L2 reads:                           0\n");
+        printf(" k. number of L2 read misses:                     0\n");
+        printf(" l. number of L2 writes:                          0\n");
+        printf(" m. number of L2 write misses:                    0\n");
+        printf(" n. L2 miss rate:                            0.0000\n");
+        printf(" o. number of writebacks from L2:                 0\n");
+        printf(" p. total memory traffic:                     %d\n",mem->read_misses+mem->write_misses+mem->writebacks);
 }
 
 void update_count(struct cache *mem, unsigned int row, unsigned int col)
@@ -237,12 +280,10 @@ void access_cache(struct cache *mem, unsigned long address, char mode)
      {
          if ( mode == 'r')
          {
-//           printf("Read Hit!\n");
              mem->read_hits++;
          }
          else
          {
-//           printf("Write Hit!\n");
              mem->write_hits++;
          }
      }
@@ -250,12 +291,10 @@ void access_cache(struct cache *mem, unsigned long address, char mode)
      {
          if ( mode == 'r')
          {
-//           printf("Read Miss!\n");
              mem->read_misses++;
          }
          else
          {
-//           printf("Write Miss!\n");
              mem->write_misses++;
          }
 
@@ -274,7 +313,6 @@ void access_cache(struct cache *mem, unsigned long address, char mode)
      if(mode == 'w')
         mem->blocks[index*mem->assoc + column].dirty = 1;
 
-//   assert( mode == 'w' && mem->blocks[index*mem->assoc + column].dirty == 1);
      return;
 }
 
@@ -302,8 +340,12 @@ int create_space(struct cache *mem, unsigned long tag, unsigned long index)
         }
      }
      assert(j <= (mem->assoc - 1));
-
-     //if valid call evict...essentially access upper memory
+    
+     if(mem->blocks[row+j].valid == 1 && mem->blocks[row+j].dirty == 1 )
+     {
+        //if valid call evict...essentially access upper memory
+        mem->writebacks++;
+     }
 
      mem->blocks[row + j].tag = 0;
      mem->blocks[row + j].valid = 0;
